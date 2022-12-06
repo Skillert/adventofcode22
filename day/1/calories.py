2ddc764ab47ced4fa1484fc82773c16963885381
Peter Mollberg <peter.mollberg@starstable.com>
2022-12-06 14:35:46 +0100

    Rename function for better description

--- day/1/calories.py
+++ day/1/calories.py
@@ -1,21 +1,21 @@
 import sys
 
-def read_input(filename):
+def max_calories(filename):
     # Create an empty list
     calories = []
 
     # Open file
     with open(filename, 'r') as data:
         # Set sum to 0
-        sum = 0
+        calories_sum = 0
 
         # For each line in the input file sum the values together unless it's a newline, then we add the value to our list and go through next
         for line in data:
             if line[0] != '\n':
-                sum += int(line)
+                calories_sum += int(line)
             else:
-                calories.append(sum)
-                sum = 0
+                calories.append(calories_sum)
+                calories_sum = 0
 
     return calories
 
@@ -34,7 +34,7 @@ def find_top_elfs(calories):
 
 if len(sys.argv) == 2:
     # Read input
-    calories = read_input(sys.argv[1])
+    calories = max_calories(sys.argv[1])
 
     # Find top 3 calorie elfs
     top_elfs = find_top_elfs(calories)
