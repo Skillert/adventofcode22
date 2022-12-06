a9369453cc914669a8f5d639924c804598ef0206
Peter Mollberg <peter.mollberg@starstable.com>
2022-12-06 14:29:50 +0100

    Refactored day 1

--- day/1/calories.py
+++ day/1/calories.py
@@ -1,44 +1,45 @@
 import sys
 
-def readInput(filename):
-    # Set sum to 0
-    sum = 0
-
+def read_input(filename):
     # Create an empty list
     calories = []
+
+    # Open file
+    with open(filename, 'r') as data:
+        # Set sum to 0
+        sum = 0
+
+        # For each line in the input file sum the values together unless it's a newline, then we add the value to our list and go through next
+        for line in data:
+            if line[0] != '\n':
+                sum += int(line)
+            else:
+                calories.append(sum)
+                sum = 0
+
+    return calories
+
+def find_top_elfs(calories):
+    # Sort calories in descending order
+    calories.sort(reverse=True)
 
     # Top three calorie elfs
     top_elfs = 0
 
-    # Open file
-    data = open(filename,'r')
-
-    # For each line in the input file sum the values together unless it's a newline, then we add the value to our list and go through next
-    for line in data.readlines():
-        if (line[0] != '\n'):
-            sum+=int(line)
-        else:
-            calories.append(sum)
-            sum = 0
-            continue
-
-    # Sort calories
-    calories.sort()
-
-    print(calories)
-    print(f'King elf: {max(calories)}')
-
     # Add top 3 calorie elfs to our list
     for elf in range(3):
-        top_elfs+=max(calories)
-        calories.pop()
+        top_elfs += calories[elf]
 
-    print(f'The three kings: {top_elfs}')
-
-    # Close file
-    data.close()
+    return top_elfs
 
 if len(sys.argv) == 2:
-    lines = readInput(sys.argv[1])
+    # Read input
+    calories = read_input(sys.argv[1])
+
+    # Find top 3 calorie elfs
+    top_elfs = find_top_elfs(calories)
+
+    print(f'King elf: {max(calories)}')
+    print(f'The three kings: {top_elfs}')
 else:
     print("calories.py inputfile")
